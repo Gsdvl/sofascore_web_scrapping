a84a3f8884d7be694a7cdddaeb0250b56033477c
Gsdvl <gsdvl2004@gmail.com>
2024-10-24 16:42:14 -0300

    Web scraper funcional, ainda não salva

--- web_scraper.py
+++ web_scraper.py
@@ -1,28 +1,35 @@
 import requests
 from datetime import datetime, timedelta
+import re
+import copy
 
 
 date = []
 error_msg = "{'error': {'code': 404, 'message': 'Not Found'}}{'error': {'code': 404, 'message': 'Not Found'}}"
 
 
+def normal_case_to_snake_case(text):
+    # Remove espaços extras e converte para snake_case
+    return re.sub(r'\s+', '_', text.strip()).lower()
+
+
 match_info_keys = ['match_id', 'home_team', 'away_team', 'year', 'tournament', 'season', 'home_score', 'away_score']
 match_info_dict = dict.fromkeys(match_info_keys)
 match_info = []
 
-# match_overview_keys = ['match_id',
-#                       'home_ball_possession', 'away_ball_possession',
-#                       'home_big_chances', 'away_big_chances',
-#                       'home_total_shots', 'away_total_shots',
-#                       'home_goalkeeper_saves', 'away_goalkeeper_saves',
-#                       'home_corner_kicks', 'away_corner_kicks',
-#                       'home_fouls', 'away_fouls',
-#                       'home_passes', 'away_passes',
-#                       'home_tackles', 'away_tackles',
-#                       'home_free_kicks', 'away_free_kicks',
-#                       'home_yellow_cards', 'away_yellow_cards']
-# match_overview_dict = dict.fromkeys(match_overview_keys)
-# match_overview = []
+match_overview_keys = ['match_id',
+                       'home_ball_possession', 'away_ball_possession',
+                       'home_big_chances', 'away_big_chances',
+                       'home_total_shots', 'away_total_shots',
+                       'home_goalkeeper_saves', 'away_goalkeeper_saves',
+                       'home_corner_kicks', 'away_corner_kicks',
+                       'home_fouls', 'away_fouls',
+                       'home_passes', 'away_passes',
+                       'home_tackles', 'away_tackles',
+                       'home_free_kicks', 'away_free_kicks',
+                       'home_yellow_cards', 'away_yellow_cards']
+match_overview_dict = dict.fromkeys(match_overview_keys)
+match_overview = []
 
 shots_keys = ['match_id',
               'home_total_shots', 'away_total_shots',
@@ -76,18 +83,14 @@ goalkeeping_dict = dict.fromkeys(goalkeeping_keys)
 goalkeeping = []
 
 
-def dates_from_2008():
-    start_date = datetime(2008, 1, 1)
+def dates_from_year(year):
+    start_date = datetime(year, 1, 1)
     end_date = datetime(2024, 10, 16)
 
     temp_date = start_date
     while temp_date <= end_date:
         date.append(temp_date.strftime('%Y-%m-%d'))
         temp_date += timedelta(days=1)
-
-
-print("Gerando datas...")
-dates_from_2008()
 
 
 def get_info_page(date):
@@ -148,18 +151,57 @@ def get_event_info(page):
             print('=================================================')
             statistics_page = get_statistics_page(id)
             print(f"ID DA PAGINA: {id}")
-            if statistics_page != error_msg:
+            if 'error' not in statistics_page:
                 get_statistics(statistics_page)
             # print('=========================================================')
             match_info.append(match_info_dict)
 
 
+def process_statistics(category, stats_list, dict):
+    stats_dict = copy.deepcopy(dict)
+    for item in category['statisticsItems']:
+        print("NOME DO ITEM A SER COLETADO:"+item['name'])
+        key_home = 'home_' + normal_case_to_snake_case(item['name'])
+        key_away = 'away_' + normal_case_to_snake_case(item['name'])
+        stats_dict[key_home] = item['home']
+        stats_dict[key_away] = item['away']
+    stats_list.append(stats_dict)
+    print(item['name'] + ' collected')
+
+
 def get_statistics(page):
     for category in page['statistics'][0]['groups']:
-        print(category['groupName'])
+        if category['groupName'] == 'Match overview':
+            print("Coletando overview")
+            process_statistics(category, match_overview, match_overview_dict)
+        elif category['groupName'] == 'Shots':
+            print("Coletando chutes...")
+            process_statistics(category, shots, shots_dict)
+        elif category['groupName'] == 'Attack':
+            print("Coletando ataques...")
+            process_statistics(category, attack, attack_dict)
+        elif category['groupName'] == 'Passes':
+            print("Coletando passes")
+            process_statistics(category, passes, passes_dict)
+        elif category['groupName'] == 'Duels':
+            print("Coletando duelos...")
+            process_statistics(category, duels, duels_dict)
+        elif category['groupName'] == 'Defending':
+            print("Coletando defesas...")
+            process_statistics(category, defending, defending_dict)
+        elif category['groupName'] == 'Goalkeeping':
+            print("Coletando informações do goleiro...")
+            process_statistics(category, goalkeeping, goalkeeping_dict)
+        else:
+            print("Unknown category collected")
 
 
-# get_statistics(get_statistics_page(12921285))
-print("obtendo id de eventos...")
-get_event_info(get_info_page(date[6000]))
+def main():
+    dates_from_year(2016)
+    for date_idx in range(0, len(date)):
+        get_event_info(get_info_page(date[date_idx]))
 
+
+if __name__ == '__main__':
+    main()
+    print("Sofascore Raspado")
